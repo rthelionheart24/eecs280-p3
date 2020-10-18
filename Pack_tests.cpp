10c593eace27b2c9ed93b65176db4abd94d0c45f
Qifa Wang <qifwang@umich.edu>
2020-10-17 21:27:31 -0400

    minor fix

--- Pack_tests.cpp
+++ Pack_tests.cpp
@@ -10,23 +10,23 @@ using namespace std;
 TEST(test_pack_default_ctor)
 {
     Pack *pack = new Pack();
-    Card *first = &pack->deal_one();
-    Card *second = &pack->deal_one();
-    Card *third = &pack->deal_one();
-    Card *fourth = &pack->deal_one();
-    ASSERT_TRUE(Card::RANK_NINE == first->get_rank() && Card::SUIT_SPADES == first->get_suit());
-    ASSERT_TRUE(Card::RANK_NINE == second->get_rank() && Card::SUIT_HEARTS == second->get_suit());
-    ASSERT_TRUE(Card::RANK_NINE == third->get_rank() && Card::SUIT_CLUBS == third->get_suit());
-    ASSERT_TRUE(Card::RANK_NINE == fourth->get_rank() && Card::SUIT_DIAMONDS == fourth->get_suit());
+    Card first = pack->deal_one();
+    Card second = pack->deal_one();
+    Card third = pack->deal_one();
+    Card fourth = pack->deal_one();
+    ASSERT_TRUE(Card::RANK_NINE == first.get_rank() && Card::SUIT_SPADES == first.get_suit());
+    ASSERT_TRUE(Card::RANK_NINE == second.get_rank() && Card::SUIT_HEARTS == second.get_suit());
+    ASSERT_TRUE(Card::RANK_NINE == third.get_rank() && Card::SUIT_CLUBS == third.get_suit());
+    ASSERT_TRUE(Card::RANK_NINE == fourth.get_rank() && Card::SUIT_DIAMONDS == fourth.get_suit());
 }
 
 TEST(test_deal_one_simple)
 {
     Pack *pack = new Pack();
-    Card *first = new Card("Nine", "Spades");
-    Card *second = new Card("Nine", "Hearts");
-    ASSERT_TRUE(pack->deal_one() == *first);
-    ASSERT_TRUE(pack->deal_one() == *second);
+    Card first("Nine", "Spades");
+    Card second("Nine", "Hearts");
+    ASSERT_TRUE(pack->deal_one() == first);
+    ASSERT_TRUE(pack->deal_one() == second);
 }
 //这个reset（）不太好测，所有其他fuction都会用reset（），应该没啥大问题
 
@@ -43,10 +43,10 @@ TEST(test_shuffle_simple)
 {
     Pack *pack = new Pack();
     pack->shuffle();
-    Card *first = new Card("Queen", "Spades");
-    Card *second = new Card("Nine", "Spades");
-    ASSERT_TRUE(*first == pack->deal_one());
-    ASSERT_TRUE(*second == pack->deal_one());
+    Card first("Queen", "Spades");
+    Card second("Nine", "Spades");
+    ASSERT_TRUE(first == pack->deal_one());
+    ASSERT_TRUE(second == pack->deal_one());
 }
 
 TEST(test_empty_simple)
